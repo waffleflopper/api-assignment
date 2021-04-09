1dc78416973b9572f418f45aafe479634cefd1e1
Robert Baddeley <robertbaddeley@gmail.com>
2021-04-09 23:52:42 +0900

    initial commit

---
+++ pages/api/config.js
@@ -0,0 +1,3 @@
+export const config = {
+    apiKey: '1ce86bdb1f15850b8bf6b98e5858f5ca',
+}
--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,52 @@
 import Head from 'next/head'
+import { useEffect, useState } from 'react'
 import styles from '../styles/Home.module.css'
+import { config } from './api/config'
 
 export default function Home() {
+
+  const [zip, setZip] = useState('94954')
+  const [weather, setWeather] = useState({})
+  const [locName, setLocName] = useState('')
+  const [newZip, setNewZip] = useState('')
+
+
+  useEffect(() => {
+      const fetchData = async () => {
+        try {
+          const result = await fetch(`https://api.openweathermap.org/data/2.5/weather?zip=${zip},us&appid=${config.apiKey}`)
+          const data = await result.json()
+
+          setLocName(data.name)
+          setWeather(data.main)
+        } catch(e) {
+          console.log(e)
+        }
+        
+    }
+
+    fetchData()
+  }, [zip])
+
+  const handleZip = (val) => {
+    if (val.length === 5) setZip(val)
+  }
+
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>Weather for {locName}</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
+      <input
+        type='text'
+        defaultValue={zip}
+        placeholder='5-Digit Zip Code' 
+        onChange={e => handleZip(e.target.value)}
+        />
+      
+       {locName}
     </div>
   )
 }
